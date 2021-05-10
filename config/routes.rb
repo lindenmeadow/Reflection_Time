2bead90d943815230308abe13a8d06acac5effa3
Evan B <konros@yahoo.com>
2021-05-10 14:26:59 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   resources :reflections
-  resources :users
 
   get 'sessions/new'
-  get '/signup' => 'users#new'
-  post '/signup' => 'users#create'
-  get '/users/:id' => 'users#show'
-  get '/users/edit' => 'users#edit'
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   delete '/signout' => 'sessions#destroy'
