79733848be255b324b038b5912b760d9678f2f18
Evan B <konros@yahoo.com>
2021-05-10 14:24:23 -0400

    Delete user.rb

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  has_many :reflections
-  validates :username, presence: true
-  validates :username, uniqueness: true
-  validates :username, length: 4..10
-  validates :password, presence: true
-  validates :password, length: 4..12
-end
